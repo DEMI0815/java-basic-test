603cdc5e75b1d130a033fcde5ab9d4873d48966a
DEMI0815 <807205251@qq.com>
2019-03-24 11:16:10 +0800

    update PracticeB of same elements in two collections

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class PracticeB {
+class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> resultList = new ArrayList<>();
+        for (List<String> collection2List : collection2) {
+            resultList.addAll(collection1.stream().filter(collection2List::contains).collect(Collectors.toList()));
+        }
+        resultList = new ArrayList<>(new LinkedHashSet<>(resultList));
+        return resultList;
     }
 }
