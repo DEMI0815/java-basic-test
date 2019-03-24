47f9402c90c326da46ca5cacfe9fd7ecb8f40ab1
DEMI0815 <807205251@qq.com>
2019-03-24 20:34:19 +0800

    update PracticeB of same elements and count

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,31 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> resultMap = new HashMap<>();
+        for(String item : collection1) {
+            if(item.contains("-")) {
+                String key = item.split("-")[0];
+                Integer value = Integer.valueOf(item.split("-")[1]);
+                if(!resultMap.containsKey(key)){
+                    resultMap.put(key, value);
+                } else {
+                    resultMap.replace(key, resultMap.get(key) + value);
+                }
+            }else {
+                if (!resultMap.containsKey(item)) {
+                    resultMap.put(item, 1);
+                } else {
+                    resultMap.replace(item, resultMap.get(item) + 1);
+                }
+            }
+        }
 
-        return null;
+        return resultMap;
     }
 }
