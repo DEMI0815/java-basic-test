d1938c2e58a885610d6dda283dca4628b98d83f2
DEMI0815 <807205251@qq.com>
2019-03-25 14:23:38 +0800

    update PracticeA of a new map with element and count

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> resultCollection = collectionA;
+        List<String> collectionBList = collectionB.get("value");
+        for (String item : resultCollection.keySet()) {
+            if (collectionBList.contains(item)) {
+                resultCollection.replace(item, resultCollection.get(item) - 1);
+            }
+        }
 
-        return null;
+        return resultCollection;
     }
 }
