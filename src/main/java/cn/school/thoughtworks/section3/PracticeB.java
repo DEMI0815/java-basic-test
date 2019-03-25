211ef9b3e5764e2bc003397f7770709ecf823182
DEMI0815 <807205251@qq.com>
2019-03-25 14:43:47 +0800

    update PracticeB of a new map with element and count

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,21 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
+    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> resultCollection = collectionA;
+        List<String> collectionBList = collectionB.get("value");
+        for (String item : resultCollection.keySet()) {
+            if (collectionBList.contains(item)) {
+                resultCollection.replace(item, everyThreeMinusOne(item, resultCollection));
+            }
+        }
 
-        return null;
+        return resultCollection;
+    }
+
+    private Integer everyThreeMinusOne(String key, Map<String, Integer> collection) {
+        int value = collection.get(key);
+        return value - value / 3;
     }
 }
