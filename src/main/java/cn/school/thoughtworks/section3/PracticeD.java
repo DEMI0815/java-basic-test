7d3970f5a8b3e1d026628105d2e80fd8aae8d307
DEMI0815 <807205251@qq.com>
2019-03-25 15:20:53 +0800

    update PracticeD of a new map with element and count

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,35 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+import cn.school.thoughtworks.section2.PracticeB;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> resultMap = sameElementMap(collectionA);
+        List<String> collectionBList = collectionB.get("value");
 
-        return null;
+        for (String item : resultMap.keySet()) {
+            if (collectionBList.contains(item)) {
+                resultMap.replace(item, everyThreeMinusOne(item, resultMap));
+            }
+        }
+
+        return resultMap;
+    }
+
+    private Map<String, Integer> sameElementMap(List<String> list) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        PracticeB.sameElement(list, resultMap);
+        return resultMap;
+    }
+
+    private Integer everyThreeMinusOne(String key, Map<String, Integer> collection) {
+        int value = collection.get(key);
+        return value - value / 3;
     }
 }
