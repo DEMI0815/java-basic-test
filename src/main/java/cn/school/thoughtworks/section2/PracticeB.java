57180ac2f042e0e9dcaaf9dc253e7d4d490dc90a
DEMI0815 <807205251@qq.com>
2019-03-25 15:10:45 +0800

    add static method of sameElement in PracticeB

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,5 +1,7 @@
 package cn.school.thoughtworks.section2;
 
+import cn.school.thoughtworks.section3.PracticeD;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +10,13 @@ public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
         Map<String, Integer> resultMap = new HashMap<>();
-        for(String item : collection1) {
+        sameElement(collection1, resultMap);
+
+        return resultMap;
+    }
+
+    public static void sameElement(List<String> list, Map<String, Integer> resultMap) {
+        for(String item : list) {
             if(item.contains("-")) {
                 String key = item.split("-")[0];
                 Integer value = Integer.valueOf(item.split("-")[1]);
@@ -25,7 +33,5 @@ public class PracticeB {
                 }
             }
         }
-
-        return resultMap;
     }
 }
