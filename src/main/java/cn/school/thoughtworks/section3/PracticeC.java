942c2b779707a2c908980ae01389783402736a77
DEMI0815 <807205251@qq.com>
2019-03-25 15:23:39 +0800

    integrate some duplicate code

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,5 +1,7 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +9,8 @@ import java.util.Map;
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> collectionB) {
         //实现练习要求，并改写该行代码。
-        Map<String, Integer> resultMap = sameElementMap(collectionA);
+        PracticeA s2PracticeA = new PracticeA();
+        Map<String, Integer> resultMap = s2PracticeA.countSameElements(collectionA);
         List<String> collectionBList = collectionB.get("value");
 
         for (String item : resultMap.keySet()) {
@@ -16,18 +19,6 @@ public class PracticeC {
             }
         }
 
-        return resultMap;
-    }
-
-    private Map<String, Integer> sameElementMap(List<String> list) {
-        Map<String, Integer> resultMap = new HashMap<>();
-        for(String item : list) {
-            if(!resultMap.containsKey(item)) {
-                resultMap.put(item, 1);
-            } else {
-                resultMap.replace(item, resultMap.get(item) + 1);
-            }
-        }
         return resultMap;
     }
 
